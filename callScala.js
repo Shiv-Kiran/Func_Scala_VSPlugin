e9c78bd17b80886f73d9ae1bff6277a0322cce66
Heemmanshuu Dasari <70682554+heemmanshuu@users.noreply.github.com>
2023-05-04 00:05:23 +0530

    Update callScala.js
    Code works. One command exists where you have to select a piece of code for it to get refactored. It  must definitely need a while/for/conditional statement for it to work (in those cases, it returns NO Refactoring if there is nothing possible)

--- callScala.js
+++ callScala.js
@@ -1,61 +1,55 @@
 const vscode = require('vscode')
 const fs = require('fs'); // to read and write to files
 
-var text = "this is my code";
 
-//import * as vscode from 'vscode'
 const callbackForCommand = () => {
-
+  var text = "nothing";
   console.log("entered callback function");
-  const textEditor = vscode.window.activeTextEditor; // This is good, a global var to access the vscode process
-  //const documentText = editor.getText(); // This should give you the text in the open file
+  const textEditor = vscode.window.activeTextEditor;
+  var oritext, selectionRange;
 
-  var firstLine = textEditor.document.lineAt(0);
-  var lastLine = textEditor.document.lineAt(textEditor.document.lineCount - 1);
-  var textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+  const selection = textEditor.selection;
+  if (selection && !selection.isEmpty) {
+    selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+    oritext = textEditor.document.getText(selectionRange);
+  }
+  else{
+    vscode.window.showInformationMessage("You haven't selected anything!");
+  }
 
-  //console.log(textRange);
 
-  var oritext = textEditor.document.getText(textRange);
-
-  // Call Scala program, give documentText as input, throw the output into a file for now
-
-  fs.writeFile("C:/Users/gnana/Desktop/IDEtool/namrata/src/main/scala/temp_code.scala", oritext, (err) => {
+  fs.writeFile("C:/Users/gnana/Desktop/IDEtool/namrata/src/main/scala/temp_code.txt", oritext, (err) => {
     if (err) throw err;
     console.log('oritext has been saved into file');
   });
 
+// Call Scala program, give documentText as input, throw the output into a file for now
+  function goToScala() {
+    const exec = require('child_process').exec;
+    return new Promise((resolve, reject) => {
+        exec(`cd C:/Users/gnana/Desktop/IDEtool/namrata && sbt --error "run src/main/scala/temp_code.txt" > answers.txt && type answers.txt`,
+        (err, stdout, stderr) => {
+            text = stdout;
+            resolve(stdout);
+            if (err) {
+                console.log('error: ' + err);
+                reject();
+            }
+        });
+    });
+  }
+  const ans = goToScala();
+  ans.then( () => {
+        const workspaceEdit = new vscode.WorkspaceEdit();
+        workspaceEdit.replace(textEditor.document.uri, selectionRange, text);
 
-  const exec = require('child_process').exec;
-  exec(`cd C:/Users/gnana/Desktop/IDEtool/namrata && sbt "run src/main/scala/temp_code.scala" > answers.txt && type answers.txt`,
-   (err, stdout, stderr) => {
-    console.log('stdout: ' + stdout);
-    text = stdout;
-    console.log('stderr: ' + stderr);
-    if (err) {
-        console.log('error: ' + err);
-    }
-  });
+        vscode.workspace.applyEdit(workspaceEdit).then(() => {
+            vscode.window.showInformationMessage('refactoring done!');
+            textEditor.document.save();
+        });
+    })
+    .catch(() => {console.log("It got rejected")})
 
-  console.log("completed cp.exec");
-
-  
-  const workspaceEdit = new vscode.WorkspaceEdit(); // Can use this class to perform edits, I believe that you 
- // need the range that needs to be updated (good idea to console.log and see what these objects look like, 
- //   I suspect, (line, startCol, len)
-
-  // Make the updates
-  workspaceEdit.replace(textEditor.document.uri, textRange, text);
-
-
-  vscode.workspace.applyEdit(workspaceEdit).then(() => {
-    vscode.window.showInformationMessage(
-          'refactoring done!'
-        );
-    textEditor.document.save();
-  });
-  
-  
 };
 
 module.exports = callbackForCommand;
